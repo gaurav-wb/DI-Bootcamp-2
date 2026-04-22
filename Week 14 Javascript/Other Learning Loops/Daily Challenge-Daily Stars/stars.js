c6282cec061d55b58ee2e422ff2462114fa095ac
Gaurav-ng <neermulgn@gmail.com>
2026-04-22 18:50:52 +0400

    clear

---
+++ Week 14 Javascript/Other Learning Loops/Daily Challenge-Daily Stars/stars.js
@@ -0,0 +1,17 @@
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+let pattern = "";
+
+for (let i=0; i <6; i++) {
+    pattern += "* "
+    console.log(pattern)
+}
