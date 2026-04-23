995c93b0803755179a868941839e58da5870b388
Gaurav-ng <neermulgn@gmail.com>
2026-04-23 20:32:19 +0400

    Initial Commit

--- Week 15 JavaScript/ExercisesXP/ex5.js
+++ Week 15 JavaScript/ExercisesXP/ex5.js
@@ -36,7 +36,7 @@
 function changeEnough(itemPrice, amountOfChange) {
     let quarters = amountOfChange[0] * 0.25
     let dimes = amountOfChange[1] * 0.1
-    let nickel = amountOfChange[2] *0.05
+    let nickel = amountOfChange[2] * 0.05
     let pennies = amountOfChange[3] * 0.01
 
     let total = quarters + dimes + nickel + pennies
@@ -45,6 +45,6 @@ function changeEnough(itemPrice, amountOfChange) {
 }
 
 //testinng
-console.log(changeEnough(14.11, [2,100,0,0]))
-console.log(changeEnough(0.75, [0,0,20,5]))
+console.log(changeEnough(14.11, [2, 100, 0, 0]))
+console.log(changeEnough(0.75, [0, 0, 20, 5]))
 console.log(changeEnough(14.11, [2, 100, 0, 0]))
